160f3458623a5a1aa363505d6ee1dc6c2e246362
Oyaki <oc.yaki122@gmail.com>
2021-11-07 20:10:35 +0900

    PWMのピンを変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,53 @@
 asm(".global _printf_float");
 
 #include <encoder1.h>
+#include <encoder3.h>
 #include <mbed.h>
 
-PwmOut pwm(PA_7);
-DigitalOut dir(PA_6);
+DigitalOut dir(PA_7);
+DigitalOut dir1(PA_3);
+PwmOut pwm1(PA_1);
+PwmOut pwm0(PA_6);
+// DigitalOut dir(PA_6);
 
 DigitalOut led(LED1);
 
 DigitalIn sw(PA_10);
 
 Encoder1 encoder1(360);
+Encoder3 encoder3(360);
 
 Thread thread(osPriorityNormal, 1024);
 
 void encoder() {
   encoder1.start();
+  encoder3.start();
   while (true) {
     encoder1.update();
+    encoder3.update();
 
-    printf("angle1: %lld, %f\n", encoder1.getRawSumPulse(),
-           encoder1.getSumangle());
+    printf("angle1: %lld, %lld\n", encoder1.getRawSumPulse(),
+           encoder3.getRawSumPulse());
+    // printf("%d\n", TIM1->CNT);
     ThisThread::sleep_for(10ms);
   }
 }
 
 int main() {
-  printf("angle1: %lld", encoder1.getRawSumPulse());
-
   thread.start(callback(encoder));
+
+  pwm0.period_ms(1);
 
   while (sw) {
   }
 
   while (true) {
     dir = !dir;
-    pwm = 0.5;
+    pwm0 = 0.5;
 
     led = dir.read();
     ThisThread::sleep_for(1500ms);
-    pwm = 0;
+    pwm0 = 0;
     ThisThread::sleep_for(500ms);
   }
 }
